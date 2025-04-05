fc79d40d17ca9050fc114af9b4c893786d659dce
Pedro Tomé <pmgte@iscte-iul.pt>
2025-04-05 17:50:50 +0100

    Tests for Uploader and Validator

---
+++ src/test/java/UploadCSV/CsvUploaderTests.java
@@ -0,0 +1,28 @@
+package UploadCSV;
+
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CsvUploaderTests {
+
+    /**
+     * Tests the uploadCsv method of the CsvUploader class.
+     * Ensures that the method correctly reads a CSV file and returns non-null, non-empty data.
+     */
+    @Test
+    void UploadCsv() {
+        CsvUploader uploader = new CsvUploader();
+        String filePath = "src/main/resources/teste.csv";
+
+        try {
+            List<String[]> data = uploader.uploadCsv(filePath);
+            assertNotNull(data, "Data should not be null");
+            assertFalse(data.isEmpty(), "Data should not be empty");
+        } catch (IOException e) {
+            fail("IOException should not occur: " + e.getMessage());
+        }
+    }
+
+}
---
+++ src/test/java/UploadCSV/CsvValidatorTests.java
@@ -0,0 +1,259 @@
+package UploadCSV;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CsvValidatorTests {
+
+    CsvValidator validator;
+    static String[] exampleData;
+    static String[] validHeaders;
+    static final Path LOG_FILE_PATH = Path.of("csv_errors.log");
+
+    @BeforeAll
+    static void init() {
+        exampleData =  new String[]{"1", "12345", "67890", "100.0", "200.0", "POINT(1 1)", "Owner1", "Freguesia1", "Municipio1", "Ilha1"};
+        validHeaders = new String[]{"OBJECTID", "PAR_ID", "PAR_NUM", "Shape_Length", "Shape_Area",
+                "geometry", "OWNER", "Freguesia", "Municipio", "Ilha"};
+    }
+
+    @BeforeEach
+    void setUp() {
+        validator = new CsvValidator();
+        try {
+            Files.write(LOG_FILE_PATH, new byte[0]);
+        } catch (IOException e) {
+            fail("Failed to clear log file: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void emptyData() {
+        List<String[]> data = List.of();
+        Exception exception = assertThrows(CsvException.class, () -> {
+            validator.validate(data);
+        });
+
+        String expectedMessage = "csv file is empty!";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Nested
+    class HeaderTests {
+
+        @Test
+        void headerFewerThan10Columns() {
+            List<String[]> data = List.of(
+                    new String[]{"OBJECTID", "PAR_ID", "PAR_NUM", "Shape_Length", "Shape_Area", "geometry", "OWNER", "Freguesia", "Municipio"},
+                    exampleData
+            );
+
+            Exception exception = assertThrows(CsvException.class, () -> {
+                validator.validate(data);
+            });
+
+            String actualMessage = exception.getMessage();
+            assertTrue(actualMessage.contains("incomplete or invalid headers."));
+        }
+
+        @Test
+        void headerMoreThan10Columns() {
+            List<String[]> data = List.of(
+                    new String[]{"OBJECTID", "PAR_ID", "PAR_NUM", "Shape_Length", "Shape_Area", "geometry", "OWNER", "Freguesia", "Municipio", "Ilha", "ExtraColumn"},
+                    exampleData
+            );
+
+            Exception exception = assertThrows(CsvException.class, () -> {
+                validator.validate(data);
+            });
+
+            String actualMessage = exception.getMessage();
+            assertTrue(actualMessage.contains("incomplete or invalid headers."));
+        }
+
+        @Test
+        void headersAreCorrect() {
+            List<String[]> data = List.of(validHeaders, exampleData);
+
+            assertDoesNotThrow(() -> validator.validate(data));
+        }
+
+        @Test
+        void headerObjectIdIsIncorrect() {
+            List<String[]> data = List.of(
+                    new String[]{"BAD_OBJECTID", "PAR_ID", "PAR_NUM", "Shape_Length", "Shape_Area",
+                            "geometry", "OWNER", "Freguesia", "Municipio", "Ilha"}, exampleData
+            );
+
+            CsvException exception = assertThrows(CsvException.class, () -> validator.validate(data));
+            assertTrue(exception.getMessage().contains("invalid headers"));
+        }
+
+        @Test
+        void headerParIdIsIncorrect() {
+            List<String[]> data = List.of(
+                    new String[]{"OBJECTID", "BAD_PAR_ID", "PAR_NUM", "Shape_Length", "Shape_Area",
+                            "geometry", "OWNER", "Freguesia", "Municipio", "Ilha"}, exampleData
+            );
+
+            CsvException exception = assertThrows(CsvException.class, () -> validator.validate(data));
+            assertTrue(exception.getMessage().contains("invalid headers"));
+        }
+
+        @Test
+        void headerParNumIsIncorrect() {
+            List<String[]> data = List.of(
+                    new String[]{"OBJECTID", "PAR_ID", "BAD_PAR_NUM", "Shape_Length", "Shape_Area",
+                            "geometry", "OWNER", "Freguesia", "Municipio", "Ilha"}, exampleData
+            );
+
+            CsvException exception = assertThrows(CsvException.class, () -> validator.validate(data));
+            assertTrue(exception.getMessage().contains("invalid headers"));
+        }
+
+        @Test
+        void headerShapeLengthIsIncorrect() {
+            List<String[]> data = List.of(
+                    new String[]{"OBJECTID", "PAR_ID", "PAR_NUM", "BAD_Shape_Length", "Shape_Area",
+                            "geometry", "OWNER", "Freguesia", "Municipio", "Ilha"}, exampleData
+            );
+
+            CsvException exception = assertThrows(CsvException.class, () -> validator.validate(data));
+            assertTrue(exception.getMessage().contains("invalid headers"));
+        }
+
+        @Test
+        void headerShapeAreaIsIncorrect() {
+            List<String[]> data = List.of(
+                    new String[]{"OBJECTID", "PAR_ID", "PAR_NUM", "Shape_Length", "BAD_Shape_Area",
+                            "geometry", "OWNER", "Freguesia", "Municipio", "Ilha"}, exampleData
+            );
+
+            CsvException exception = assertThrows(CsvException.class, () -> validator.validate(data));
+            assertTrue(exception.getMessage().contains("invalid headers"));
+        }
+
+        @Test
+        void headerGeometryIsIncorrect() {
+            List<String[]> data = List.of(
+                    new String[]{"OBJECTID", "PAR_ID", "PAR_NUM", "Shape_Length", "Shape_Area",
+                            "BAD_geometry", "OWNER", "Freguesia", "Municipio", "Ilha"}, exampleData
+            );
+
+            CsvException exception = assertThrows(CsvException.class, () -> validator.validate(data));
+            assertTrue(exception.getMessage().contains("invalid headers"));
+        }
+
+        @Test
+        void headerOwnerIsIncorrect() {
+            List<String[]> data = List.of(
+                    new String[]{"OBJECTID", "PAR_ID", "PAR_NUM", "Shape_Length", "Shape_Area",
+                            "geometry", "BAD_OWNER", "Freguesia", "Municipio", "Ilha"}, exampleData
+            );
+
+            CsvException exception = assertThrows(CsvException.class, () -> validator.validate(data));
+            assertTrue(exception.getMessage().contains("invalid headers"));
+        }
+
+        @Test
+        void headerFreguesiaIsIncorrect() {
+            List<String[]> data = List.of(
+                    new String[]{"OBJECTID", "PAR_ID", "PAR_NUM", "Shape_Length", "Shape_Area",
+                            "geometry", "OWNER", "BAD_Freguesia", "Municipio", "Ilha"}, exampleData
+            );
+
+            CsvException exception = assertThrows(CsvException.class, () -> validator.validate(data));
+            assertTrue(exception.getMessage().contains("invalid headers"));
+        }
+
+        @Test
+        void headerMunicipioIsIncorrect() {
+            List<String[]> data = List.of(
+                    new String[]{"OBJECTID", "PAR_ID", "PAR_NUM", "Shape_Length", "Shape_Area",
+                            "geometry", "OWNER", "Freguesia", "BAD_Municipio", "Ilha"}, exampleData
+            );
+
+            CsvException exception = assertThrows(CsvException.class, () -> validator.validate(data));
+            assertTrue(exception.getMessage().contains("invalid headers"));
+        }
+
+        @Test
+        void headerIlhaIsIncorrect() {
+            List<String[]> data = List.of(
+                    new String[]{"OBJECTID", "PAR_ID", "PAR_NUM", "Shape_Length", "Shape_Area",
+                            "geometry", "OWNER", "Freguesia", "Municipio", "BAD_Ilha"}, exampleData
+            );
+
+            CsvException exception = assertThrows(CsvException.class, () -> validator.validate(data));
+            assertTrue(exception.getMessage().contains("invalid headers"));
+        }
+    }
+
+    @Nested
+    class DataRowTests {
+
+        @Test
+        void dataRowFewerThan10Columns() {
+            List<String[]> data = List.of( validHeaders,
+                    new String[]{"1", "12345", "67890", "100.0", "200.0", "POINT(1 1)", "Owner1", "Freguesia1"}
+            );
+            assertDoesNotThrow(() -> validator.validate(data));
+        }
+
+        @Test
+        void dataRowMoreThan10Columns() {
+            List<String[]> data = List.of(validHeaders,
+                    new String[]{"1", "12345", "67890", "100.0", "200.0", "POINT(1 1)", "Owner1", "Freguesia1", "Municipio1", "Ilha1", "ExtraColumn"}
+            );
+
+            assertDoesNotThrow(() -> validator.validate(data));
+        }
+
+        @Test
+        void parIdIsInvalid() {
+            List<String[]> data = List.of(validHeaders,
+                    new String[]{"1", "INVALID_PAR_ID", "67890", "100.0", "200.0", "POINT(1 1)", "Owner1", "Freguesia1", "Municipio1", "Ilha1"}
+            );
+
+            assertDoesNotThrow(() -> validator.validate(data));
+        }
+
+        @Test
+        void shapeLengthIsInvalid() {
+            List<String[]> data = List.of(validHeaders,
+                    new String[]{"1", "12345", "67890", "INVALID_Shape_Length", "200.0", "POINT(1 1)", "Owner1", "Freguesia1", "Municipio1", "Ilha1"}
+            );
+
+            assertDoesNotThrow(() -> validator.validate(data));
+        }
+
+        @Test
+        void shapeAreaIsInvalid() {
+            List<String[]> data = List.of(validHeaders,
+                    new String[]{"1", "12345", "67890", "100.0", "INVALID_Shape_Area", "POINT(1 1)", "Owner1", "Freguesia1", "Municipio1", "Ilha1"}
+            );
+
+            assertDoesNotThrow(() -> validator.validate(data));
+        }
+
+        @Test
+        void geometryIsInvalid() {
+            List<String[]> data = List.of(validHeaders,
+                    new String[]{"1", "12345", "67890", "100.0", "200.0", "INVALID_GEOMETRY", "Owner1", "Freguesia1", "Municipio1", "Ilha1"}
+            );
+
+            assertDoesNotThrow(() -> validator.validate(data));
+        }
+    }
+
+}
