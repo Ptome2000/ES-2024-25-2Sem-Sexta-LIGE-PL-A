66c292a4dc4ae797cc0b6a066f0fe3b5313df443
Pedro Tomé <pmgte@iscte-iul.pt>
2025-04-24 01:14:10 +0100

    Missing javadoc

--- src/main/java/Regions/Region.java
+++ src/main/java/Regions/Region.java
@@ -1,5 +1,9 @@
 package Regions;
 
+/**
+ * The Region interface represents a geographical region with a name.
+ * It is implemented by various classes representing different levels of regions.
+ */
 public interface Region {
     String name();
 }
